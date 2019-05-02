c90bd8fdf2bd630ecb1e806649058199fae349e3
Mikael Borg <mikael.borg@nbis.se>
2019-05-02 17:21:30 +0200

    Cleanup.

--- lib/mirish/ride.rb
+++ lib/mirish/ride.rb
@@ -4,8 +4,6 @@ require 'dm-validations'
 require 'dm-aggregates'
 require 'dm-migrations'
 require 'dm-constraints'
-require 'ostruct'
-require 'sanitize'
 
 module Mirish
   # Rides 
@@ -23,16 +21,6 @@ module Mirish
 
     has n, :seats, constraint: :destroy
 
-
-    #def initialize(params = nil)
-      #self.uuid = SecureRandom.urlsafe_base64(n=32)
-      #self.adminuuid = SecureRandom.urlsafe_base64(n=32)
-      #if(params)
-      #self.title = Sanitize.clean(params[:title])
-      #self.description = Sanitize.clean(params[:description])
-      #self.date = params[:date]
-      #end
-    #end
 
     def expirydate
       return self.date + Mirish::Application.settings.expiration_time
